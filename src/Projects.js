5c0799928241594706acc95693016560f7700b5a
Stella Chung <stll.chung@gmail.com>
2017-10-27 23:31:37 -0400

    Added fadeIn transition for Projects. Moved project information into a separate JS file.

--- src/Projects.js
+++ src/Projects.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import './Projects.css';
 import SectionHeader from './SectionHeader';
+import projectInfo from './projectData';
 import Scroll from 'react-scroll';
 const Element = Scroll.Element;
 
 const Project = (props) => {
   const { name, description, imageURL, github, demo, presentation, orientation, altText } = props.info;
+  const { animation } = props;
+
   return (
-    <div className="project-card">
+    <div className={`project-card ${animation}`}>
       <div>
         <div className="project-img-container">
           <img className={orientation} src={imageURL} alt={altText} />
@@ -31,66 +34,36 @@ const Project = (props) => {
 
     </div>
   );
-}
+};
+
 class Projects extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      projectInfo: [
-        {
-          name: "Grace's Bakery",
-          description: 'A fictional e-commerce site for delivering freshly baked goods. Built with React, Redux, Express, Sequelize and PostgreSQL.',
-          imageURL: './images/gracesBakery.jpg',
-          altText: "Grace's Bakery screenshot",
-          orientation: 'portrait',
-          github: 'https://github.com/stella-yc/graces-bakery',
-          demo: 'https://gracesbakery.herokuapp.com/',
-          presentation: null
-        },
-        {
-          name: 'Forest Adventure',
-          description: 'A classic 2D platformer built with Phaser.js, ES6 and webpack. Shoot stars, plant & climb trees, and collect all 5 gems! Created as part of an independent Stackathon project at Fullstack Academy.',
-          imageURL: './images/shootingStar.gif',
-          altText: 'Gameplay featuring the ability to shoot stars.',
-          orientation: 'landscape',
-          github: 'https://github.com/stella-yc/forest-adventure',
-          demo: null,
-          presentation: 'https://www.youtube.com/watch?v=XOOIo-aKyUA'
-        },
-        {
-          name: 'Tama-Do',
-          description: "A gamified productivity app with virtual pets placed at locations of the user's choosing in the real world. Built by a team of four developers, using React Native and Firebase. Winner of Fullstack Academy's Developer's Choice Award.",
-          imageURL: './images/monsterMap.png',
-          altText: 'A virtual pet monster in Tama-do.',
-          orientation: 'landscape',
-          github: 'https://github.com/Tama-Do/tama-do',
-          demo: null,
-          presentation: 'https://www.youtube.com/watch?v=fH4YrnoB3-M&list=PLx0iOsdUOUmnCoQp2wnimmDBx22j2vXBU&index=3'
-        },
-        {
-          name: 'Weather App',
-          description: "A web app that displays local weather information and different pictures of dogs depending on the forecast. Built using jQuery as part of a freeCodeCamp project.",
-          imageURL: './images/doggyWeather.png',
-          altText: 'A doggy weather app',
-          orientation: 'landscape',
-          github: 'https://github.com/stella-yc/weather-app',
-          demo: 'https://stella-yc.github.io/weather-app/',
-          presentation: null
-        },
-        {
-          name: "Conway's Game of Life",
-          description: "A web app that allows users to set up the initial state of the game by clicking on tiles, or generate a random start state. Built using pure JavaScript.",
-          imageURL: './images/gameOfLife.gif',
-          altText: 'Game of Life in action',
-          orientation: 'landscape',
-          github: 'https://github.com/stella-yc/conways-game-of-life',
-          demo: 'https://stella-yc.github.io/conways-game-of-life/'
-        }
-      ]
+      animationClass: ''
+    };
+    this.animateScroll = this.animateScroll.bind(this);
+  }
+
+  componentDidMount () {
+    window.addEventListener('scroll', this.animateScroll);
+  }
+
+  componentWillUnmount () {
+    window.removeEventListener('scroll', this.animateScroll);
+  }
+
+  animateScroll (evt) {
+    let scrollTop = evt.srcElement.scrollingElement.scrollTop;
+    if (scrollTop > 850) {
+      this.setState({ animationClass: 'fadeIn' });
+    }
+    if (scrollTop < 850) {
+      this.setState({animationClass: ''});
     }
   }
+
   render() {
-    const projectInfo = this.state.projectInfo;
     return (
       <Element name="projects-element">
         <section className="project-section">
@@ -100,7 +73,12 @@ class Projects extends Component {
           />
           <div className="projects-container">
             {projectInfo.map(project =>
-              <Project info={project} key={project.name} />)}
+              <Project
+                info={project}
+                key={project.id}
+                animation={this.state.animationClass}
+              />)
+            }
           </div>
         </section>
       </Element>
