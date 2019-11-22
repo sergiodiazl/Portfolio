56db4bd2ef11968c00b6f0dea29b2c6713eaa902
sergiodiazl <sergiodiazleguizamon@gmail.com>
2019-11-22 15:23:52 +0300

    'added icons in the skills part'

--- src/Texts.js
+++ src/Texts.js
@@ -1,97 +1,121 @@
-export const SPANISH={
-    navSkills:"Habilidades",
-    navPortfolio:"Portfolio",
-    navProjects:"Proyectos",
-    navAbout:"Acerca de mí",
-    languageButton:"English",
-    headerTitle:"Hola,soy Sergio",
-    headerSubtitle:"Soy un ingeniero en Software,me gusta la tecnología y aprender.",
-    headerSeeMore:"Ver más",
-    skillsTitle:"Habilidades para desarrollo de software",
-    projectInfo :[
-      {
-        id: 1,
-        name: 'Room resorts',
-        description: 'Sitio dedicado a mostrar las habitaciones de un hotel ficticio,realizado en react y contentful cms',
-        imageURL: './images/rooms.png',
-        altText: 'habitaciones',
-        orientation: 'portrait',
-        github: 'https://github.com/sergiodiazl/reactResortsRooms',
-        demo: 'https://resortroomsdemo.netlify.com/',
- 
-      },
-     
-      {
-        id: 2,
-        name: 'Movie Helper',
-        description: "Web app que sugiere pelicula según los parámetros del usuario.React,redux,Wow.js.Información de The movie database",
-        imageURL: './images/movie-helper.png',
-        altText: 'Movie helper app',
-        orientation: 'portrait',
-        github: 'https://github.com/sergiodiazl/redux-movie-helper',
-        demo: 'https://moviehelperr.herokuapp.com/',
-      
-      },
-        {
-          id: 3,
-          name: "React coin info",
-          description: ' Visualizador de cotizaciones y estadisticas de cripto monedas,realizado en React',
-          imageURL: './images/react-coin.png',
-          altText: "screenshot de ract coin info",
-          orientation: 'portrait',
-          github: 'https://github.com/sergiodiazl/react-coin-info',
-          demo: 'http://react-coin-info.herokuapp.com//',
-        
-        },
-      
-        {
-          id: 4,
-          name: 'TpApp',
-          description: "Aplicación móvil que permite publicar fotos con ubicación y seguir a otros usuarios,multilenguaje.Realizada en Java,xml y back end en php y mysql",
-          imageURL: './images/tpapp.png',
-          altText: 'red social con localizacion',
-          orientation: 'landscape',
-          github: 'https://github.com/sergiodiazl/tpapp',
-          demo: null,
-         
-        }
-      
-      ],
-      projectArticleInfo: [
-        {
-          name: 'Software para gestión de programas de estudio',
-          description: "Programa para coordinar y aprobar los programas de estudios propuestos.Para el uso de la Universidad Nacional Arturo Jauretche.",
-          imageURL: './images/syllabus.jpg',
-          altText: 'Equipo de desarrollo',
-          link: 'https://www.unaj.edu.ar/estudiantes-de-ingenieria-en-informatica-desarrollaron-un-software-de-gestion-de-programas-de-estudio/'
-        },
-        {
-          name: "Explicación del plegado de proteínas",
-          description: "Material didáctico acerca de las proteínas y sus procesos de plegado. Para el uso de la Universidad Nacional Arturo Jauretche.",
-          imageURL: './images/protein.png',
-          altText: "una proteina",
-          link: 'https://proteinas-unaj.herokuapp.com/'
-        },
-      
-      ],
-      aboutTitle:'Acerca de mí',
-      aboutInfo:"Soy Sergio.Soy un ingeniero en informática gradudo de la universidad Nacional Arturo Jauretche en  Argentina.Tuve educacion adicional en formas de cursos  y becas en Tucuman ,Argentina y Kuthay,Turquia.Principalmente interesado en desarrollo web pero estoy abierto a otras propuestas.",
+export const SPANISH = {
+  navSkills: "Habilidades",
+  navPortfolio: "Portfolio",
+  navProjects: "Proyectos",
+  navAbout: "Acerca de mí",
+  languageButton: "English",
+  headerTitle: "Hola,soy Sergio",
+  headerSubtitle: "Soy un ingeniero en Software,me gusta la tecnología y aprender.",
+  headerSeeMore: "Ver más",
+  skillsTitle: "Habilidades para desarrollo de software",
+  projectInfo: [
+    {
+      id: 1,
+      name: 'Clasificador de imagenes Imagenet',
+      description: "Reconoce objetos en una imagen ,servidor corriendo un modelo keras entrenado usando la base de datos imagenet,Front end en React",
+      imageURL: './images/imagenet.png',
+      altText: 'image recognizer',
+      orientation: 'portrait',
+      github: 'https://github.com/sergiodiazl/imagreconfront',
+      demo: 'https://imagenetkeras.netlify.com',
+
+    },
+
+
+    {
+      id: 2,
+      name: 'Movie Helper',
+      description: "Web app que sugiere pelicula según los parámetros del usuario.React,redux,Wow.js.Información de The movie database",
+      imageURL: './images/movie-helper.png',
+      altText: 'Movie helper app',
+      orientation: 'portrait',
+      github: 'https://github.com/sergiodiazl/redux-movie-helper',
+      demo: 'https://moviehelperr.herokuapp.com/',
+
+    },
+    {
+      id: 3,
+      name: 'Room resorts',
+      description: 'Sitio dedicado a mostrar las habitaciones de un hotel ficticio,realizado en react y contentful cms',
+      imageURL: './images/rooms.png',
+      altText: 'habitaciones',
+      orientation: 'portrait',
+      github: 'https://github.com/sergiodiazl/reactResortsRooms',
+      demo: 'https://resortroomsdemo.netlify.com/',
+
+    },
+    {
+      id: 4,
+      name: "React coin info",
+      description: ' Visualizador de cotizaciones y estadisticas de cripto monedas,realizado en React',
+      imageURL: './images/react-coin.png',
+      altText: "screenshot de ract coin info",
+      orientation: 'portrait',
+      github: 'https://github.com/sergiodiazl/react-coin-info',
+      demo: 'http://react-coin-info.herokuapp.com//',
+
+    },
+
+
+
+  ],
+  projectArticleInfo: [
+    {
+      name: 'Software para gestión de programas de estudio',
+      description: "Programa para coordinar y aprobar los programas de estudios propuestos.Para el uso de la Universidad Nacional Arturo Jauretche.",
+      imageURL: './images/syllabus.jpg',
+      altText: 'Equipo de desarrollo',
+      link: 'https://www.unaj.edu.ar/estudiantes-de-ingenieria-en-informatica-desarrollaron-un-software-de-gestion-de-programas-de-estudio/'
+    },
+    {
+      name: "Explicación del plegado de proteínas",
+      description: "Material didáctico acerca de las proteínas y sus procesos de plegado. Para el uso de la Universidad Nacional Arturo Jauretche.",
+      imageURL: './images/protein.png',
+      altText: "una proteina",
+      link: 'https://proteinas-unaj.herokuapp.com/'
+    },
+
+  ],
+  aboutTitle: 'Acerca de mí',
+  aboutInfo: "Soy Sergio.Soy un ingeniero en informática gradudo de la universidad Nacional Arturo Jauretche en  Argentina.Tuve educacion adicional en formas de cursos  y becas en Tucuman ,Argentina y Kuthay,Turquia.Principalmente interesado en desarrollo web pero estoy abierto a otras propuestas.",
 }
 
-export const ENGLISH={
-    navSkills:"Skills",
-    navPortfolio:"Portfolio",
-    navProjects:"Projects",
-    navAbout:"About me",
-    languageButton:"Español",
-    headerTitle:"Hello,I'm Sergio",
-    headerSubtitle:"I'm a Software engineer who likes technology and learning.",
-    headerSeeMore:"See more",
-    skillsTitle:"Software skills",
-    projectInfo :[
-      
-      {
-        id: 1,
+export const ENGLISH = {
+  navSkills: "Skills",
+  navPortfolio: "Portfolio",
+  navProjects: "Projects",
+  navAbout: "About me",
+  languageButton: "Español",
+  headerTitle: "Hello,I'm Sergio",
+  headerSubtitle: "I'm a Software engineer who likes technology and learning.",
+  headerSeeMore: "See more",
+  skillsTitle: "Software skills",
+  projectInfo: [
+    {
+      id: 1,
+      name: 'Imagenet image classifier',
+      description: "Image classifier,server running a keras model trained withthe imagenet database,front end running in React",
+      imageURL: './images/imagenet.png',
+      altText: 'image recognizer',
+      orientation: 'portrait',
+      github: 'https://github.com/sergiodiazl/imagreconfront',
+      demo: 'https://imagenetkeras.netlify.com',
+
+    },
+
+    {
+      id: 2,
+      name: 'Movie Helper',
+      description: "Movie suggesting web app using The movie database.Used Redux,React and Wow.js,available on 3 languages(English,Spanish and Russian)",
+      imageURL: './images/movie-helper.png',
+      altText: 'Movie helper app',
+      orientation: 'portrait',
+      github: 'https://github.com/sergiodiazl/redux-movie-helper',
+      demo: 'https://moviehelperr.herokuapp.com/',
+      presentation: null
+    },
+    {
+      id: 3,
       name: 'Resort rooms',
       description: 'Site showcasing a fictitous resorts rooms.Done in react and contentful cms',
       imageURL: './images/rooms.png',
@@ -99,58 +123,37 @@ export const ENGLISH={
       orientation: 'portrait',
       github: 'https://github.com/sergiodiazl/reactResortsRooms',
       demo: 'https://resortroomsdemo.netlify.com/',
-  },
-        {
-          id: 2,
-          name: 'Movie Helper',
-          description: "Movie suggesting web app using The movie database.Used Redux,React and Wow.js,available on 3 languages(English,Spanish and Russian)",
-          imageURL: './images/movie-helper.png',
-          altText: 'Movie helper app',
-          orientation: 'portrait',
-          github: 'https://github.com/sergiodiazl/redux-movie-helper',
-          demo: 'https://moviehelperr.herokuapp.com/',
-          presentation: null
-        },
-        {
-          id: 3,
-          name: "React coin info",
-          description: ' Cryptocurrency informain visualizer,built with React',
-          imageURL: './images/react-coin.png',
-          altText: "screenshot de ract coin info",
-          orientation: 'portrait',
-          github: 'https://github.com/sergiodiazl/react-coin-info',
-          demo: 'http://react-coin-info.herokuapp.com//',
-        },
-        {
-          id: 4,
-          name: 'TpApp',
-          description: "Mobile app for photo posting with geographical ubication.Done in  Java,xml with  php  and mysql back end",
-          imageURL: './images/tpapp.png',
-          altText: 'social photo posting',
-          orientation: 'landscape',
-          github: 'https://github.com/sergiodiazl/tpapp',
-          demo: null,
-        },
-        
-      
-      ],
-      projectArticleInfo: [
-        {
-          name: 'Development of syllabus management software',
-          description: "Program for coordinating and reviewing the syllabus of courses.For internal use in Arturo Jauretche National University",
-          imageURL: './images/syllabus.jpg',
-          altText: 'development team',
-          link: 'https://www.unaj.edu.ar/estudiantes-de-ingenieria-en-informatica-desarrollaron-un-software-de-gestion-de-programas-de-estudio/'
-        },
-        {
-          name: "Protein folding explanation",
-          description: "Quick explanation of proteins and the processes involved in their folding.Made for use of Arturo Jauretche national university",
-          imageURL: './images/protein.png',
-          altText: "a protein",
-          link: 'https://proteinas-unaj.herokuapp.com/'
-        },
-      
-      ],
-      aboutTitle:'About me',
-      aboutInfo:"My name is Sergio .I'm a software engineer,graduated from Arturo Jaurteche University,had additional education in development courses and scholarships in Tucuman,Argentina and Kutahya,Turkey.Currently interested in web development but open to other proposals.",
+    },
+    {
+      id: 4,
+      name: "React coin info",
+      description: ' Cryptocurrency informain visualizer,built with React',
+      imageURL: './images/react-coin.png',
+      altText: "screenshot de ract coin info",
+      orientation: 'portrait',
+      github: 'https://github.com/sergiodiazl/react-coin-info',
+      demo: 'http://react-coin-info.herokuapp.com//',
+    },
+
+
+  ],
+  projectArticleInfo: [
+    {
+      name: 'Development of syllabus management software',
+      description: "Program for coordinating and reviewing the syllabus of courses.For internal use in Arturo Jauretche National University",
+      imageURL: './images/syllabus.jpg',
+      altText: 'development team',
+      link: 'https://www.unaj.edu.ar/estudiantes-de-ingenieria-en-informatica-desarrollaron-un-software-de-gestion-de-programas-de-estudio/'
+    },
+    {
+      name: "Protein folding explanation",
+      description: "Quick explanation of proteins and the processes involved in their folding.Made for use of Arturo Jauretche national university",
+      imageURL: './images/protein.png',
+      altText: "a protein",
+      link: 'https://proteinas-unaj.herokuapp.com/'
+    },
+
+  ],
+  aboutTitle: 'About me',
+  aboutInfo: "My name is Sergio .I'm a software engineer,graduated from Arturo Jaurteche University,had additional education in development courses and scholarships in Tucuman,Argentina and Kutahya,Turkey.Currently interested in web development but open to other proposals.",
 }
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -28,20 +28,22 @@ class Skills extends Component {
 
   render() {
     const skills = [
-      'Javascript',
-      'Node.js',
-      'React',
-      'HTML & CSS',
-      'Python',
-      'Bootstrap',
-      'Git',
-      'Java',
-      'Android',
-      'MySql',
-      'MongoDb',
-      'Java',
-      'Redux',
+      {name:'Javascript',icon:'fab fa-js-square'},
+      {name:'Node.js',icon:'fab fa-node-js'},
+      {name:'React',icon:'fab fa-react'},
+      {name:'Redux',icon:''},
+      {name:'HTML ',icon:'fab fa-html5'},
+      {name:'CSS',icon:'fab fa-css3-alt'},
+      {name:'Python',icon:'fab fa-python'},
+      {name:'Bootstrap',icon:'fab fa-bootstrap'},
+      {name:'Git',icon:'fab fa-git-alt'},
+      {name:'Java',icon:'fab fa-java'},
+      {name:'MySql',icon:''},
+      {name:'MongoDb',icon:''},
+   
+     
     ];
+
     const{skillsTitle}=this.state.language
     return (
       <Element name="skills-element">
@@ -52,9 +54,11 @@ class Skills extends Component {
           />
           <p>{skillsTitle}</p>
           <ul className="skills">
-            {skills.map(skill =>
+            {skills.map(({name,icon}) =>
               
-              <li  key={skill}>{skill}
+              <li  key={name}>
+               <i className={ icon} aria-hidden="true"></i> 
+              {name}
                </li>)}
           </ul>
         </section>
